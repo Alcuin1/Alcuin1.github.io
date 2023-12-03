d64723cba0ab317d3e5880cca65583be956a53ae
jerekli <jerekli@tencent.com>
2023-12-03 17:02:19 +0800

    Site updated: 2023-12-03 17:02:18

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,38 +1,19 @@
-// reference: https://fancyapps.com/fancybox/3/docs/
 $(document).ready(function() {
-    $('img').each(function() {
-        if ($(this).parent().hasClass('fancybox')) return;
-        if ($(this).hasClass('nofancybox')) return;
-        var alt = this.alt;
-        $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" data-fancybox="gallery"' + ' data-caption="' + alt + '" class="fancybox"></a>');
-    });
-    $(this).find('.fancybox').each(function(){
-        $(this).attr('rel', 'article');
-    });
+  $('img').each(function() {
+    if ($(this).parent().hasClass('fancybox')) return;
+    if ($(this).parents().addBack().hasClass('nofancybox')) return;
+    var alt = this.alt;
+    if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" title="' + alt + '" class="fancybox"></a>');
+  });
+  $(this).find('.fancybox').each(function(){
+    $(this).attr('rel', 'article');
+  });
 });
 $(document).ready(function() {
-    $('[data-fancybox=gallery]').fancybox({
-        buttons : [
-          'zoom',
-          //'share',
-          'slideShow',
-          'fullScreen',
-          //'download',
-          'thumbs',
-          'close'
-        ],
-        // Transition effect between slides
-        //
-        // Possible values:
-        //   false            - disable
-        //   "fade'
-        //   "slide'
-        //   "circular'
-        //   "tube'
-        //   "zoom-in-out'
-        //   "rotate'
-        //
-        transitionEffect: "slide",
-    });
+  $("a[href$='.jpeg'],a[href$='.jpg'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']").attr('rel', 'gallery').fancybox({
+    helpers : {
+    title: { type: 'inside'}
+    }
+  });
 });
-
